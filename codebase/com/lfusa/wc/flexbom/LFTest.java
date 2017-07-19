2211dd0ca15668c873519396fa50b14f98464725
unknown <Administrator@plmtestwin.plmtestlab.com>
2017-07-19 15:49:59 +0530

    all folders are moved to this location

---
+++ codebase/com/lfusa/wc/flexbom/LFTest.java
@@ -0,0 +1,10 @@
+package com.lfusa.wc.flexbom;
+import com.lcs.wc.product.LCSProduct;
+public class LFTest
+{
+}[A
+ public static void main()[DString args[])
+{
+System.out.println("This is new java file");
+}[B
+}
